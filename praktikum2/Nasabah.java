82c7a423ce2e9bc5b3788a789dffe8e1f597e3d0
abhixyz1 <abhinayazrf@gmail.com>
2024-05-03 20:35:31 +0700

    Praktikum 2

---
+++ praktikum2/Nasabah.java
@@ -0,0 +1,24 @@
+package praktikum2;
+
+/**
+ * @author Abhinaya
+ */
+public class Nasabah {
+    String norek;
+    String nama;
+    String alamat;
+    int umur;
+    double saldo;
+
+    Nasabah(String norek, String nama, String alamat, int umur, double saldo) {
+        this.norek = norek;
+        this.nama = nama;
+        this.alamat = alamat;
+        this.umur = umur;
+        this.saldo = saldo;
+    }    
+
+    Nasabah() {
+
+    }
+}
